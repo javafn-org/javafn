3bbff9cf343eb408d6a2ec3316d55f729a19e0ae
Ben Stringer <ben.stringer@nrlssc.navy.mil>
2023-07-18 17:08:39 -0500

    Polished first pass at a complete stream indexing/enumerating option

--- src/main/java/org/javafn/tuple/Idx.java
+++ src/main/java/org/javafn/tuple/Idx.java
@@ -1,24 +1,56 @@
 package org.javafn.tuple;
 
+import java.util.Objects;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
+/**
+ * Add an index to each element in a stream.  Similar to
+ * <a href="https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.enumerate">Rust's enumerate</a>
+ * or <a href="https://docs.python.org/3/library/functions.html#enumerate">Python's enumerate</a>.
+ * This class can be compared to the JavaFn {@link Pair} except it is specialized to not box the integer index.
+ * <pre>{@code
+ * someStream.map(Idx.enumerate())
+ *      .peek(Idx.Peek( (i, item) -> println("Completing item " + i ))
+ *      ...;
+ * }</pre>
+ * or equivalently,
+ * <pre>{@code
+ * Idx.enumerate(someStream)
+ *      .peek(Idx.Peek( (i, item) -> println("Completing item " + i ))
+ *      ...;
+ * }</pre>
+ * @param <T>
+ */
 public class Idx<T> {
 
-	@FunctionalInterface
-	public interface IndexedFunction<TT, R> {
-		R accept(TT tt);
-	}
+	@FunctionalInterface public interface IndexedFunction<TT, R> { R apply(int i, TT tt); }
 
-	public static class Indexed<TT> {
+	@FunctionalInterface public interface IndexedConsumer<TT> { void accept(int i, TT tt); }
+	@FunctionalInterface public interface IndexedPredicate<TT> { boolean test(int i, TT tt); }
+
+	public static class StreamIndexer<TT> {
 		int count = 0;
 
-		Idx<TT> accept(final TT t) {
+		public Idx<TT> accept(final TT t) {
 			return new Idx<>(count++, t);
 		}
 	}
 
-	public static <TT> Function<TT, Idx<TT>> thisStream() {
-		return new Indexed<TT>()::accept;
+	public static <TT, R> Function<Idx<TT>, Idx<R>> Map(final IndexedFunction<TT, R> fn) { return idx -> idx.map(fn); }
+	public static <TT, R> Function<Idx<TT>, Idx<R>> Map(final Function<TT, R> fn) { return idx -> idx.map(fn); }
+	public static <TT, R> Function<Idx<TT>, R> MapDropIdx(final IndexedFunction<TT, R> fn) { return idx -> idx.mapDropIdx(fn); }
+	public static <TT> Consumer<Idx<TT>> Peek(final IndexedConsumer<TT> fn) { return idx -> idx.peek(fn); }
+	public static <TT> Predicate<Idx<TT>> Filter(final IndexedPredicate<TT> fn) { return idx -> idx.filter(fn); }
+
+	public static <TT> Stream<Idx<TT>> enumerate(final Stream<TT> stream) {
+		return stream.map(new StreamIndexer<TT>()::accept);
+	}
+
+	public static <TT> Function<TT, Idx<TT>> enumerate() {
+		return new StreamIndexer<TT>()::accept;
 	}
 
 	private final int i;
@@ -30,18 +62,47 @@ public class Idx<T> {
 	}
 
 	public int i() { return i; }
-
 	public T val() { return val; }
 
-	public <R> Idx<R> map(final IndexedFunction<T, R> fn) {
-		return new Idx<>(i, fn.accept(val));
-	}
+	/**
+	 * Map the value in this Idx to a new type and return a new Idx with the same index and the new value.
+	 */
+	public <R> Idx<R> map(final IndexedFunction<T, R> fn) { return new Idx<>(i, fn.apply(i, val)); }
 
-	public <R> R mapDropIdx(final IndexedFunction<T, R> fn) {
-		return fn.accept(val);
-	}
+	/**
+	 * Map the value in this Idx to a new type and return a new Idx with the same index and the new value.
+	 */
+	public <R> Idx<R> map(final Function<T, R> fn) { return new Idx<>(i, fn.apply(val)); }
+	/**
+	 * Map the value in this Idx to a new type and return the new value, dropping the index.
+	 */
+	public <R> R mapDropIdx(final IndexedFunction<T, R> fn) { return fn.apply(i, val); }
+	/**
+	 * Inspect this Idx but do not update it.
+	 */
+	public Idx<T> peek(final IndexedConsumer<T> fn) { fn.accept(i, val); return this; }
+	/**
+	 * Perform the supplied Predicate on this Idx.
+	 */
+	public boolean filter(final IndexedPredicate<T> fn) { return fn.test(i, val); }
+	/**
+	 * Inspect this Idx but do not update it, and do not return it for further processing.
+	 */
+	public void forEach(final IndexedConsumer<T> fn) { fn.accept(i, val); }
 
-	public T dropIdx() {
-		return val;
+	/**
+	 * Return the wrapped value and drop the index.
+	 */
+	public T dropIdx() { return val; }
+
+	@Override public int hashCode() { return Objects.hash(i, val); }
+	@Override
+	public boolean equals(final Object obj) {
+		if (obj instanceof Idx) {
+			final Idx<?> that = (Idx<?>) obj;
+			return i == that.i && Objects.equals(val, that.val);
+		}
+		return false;
 	}
+	@Override public String toString() { return "([" + i + "]:" + val.toString() + ")"; }
 }
--- src/test/java/org/javafn/tuple/IdxTest.java
+++ src/test/java/org/javafn/tuple/IdxTest.java
@@ -2,15 +2,62 @@ package org.javafn.tuple;
 
 import org.junit.Test;
 
+import java.util.List;
 import java.util.UUID;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
 
 public class IdxTest {
 
+
 	@Test
-	public void testIndexing() {
-		IntStream.range(0, 100).mapToObj(i -> UUID.randomUUID())
-				.map(Idx.thisStream())
-				.forEach(idx -> System.out.printf("String value %d is mapped to index %d.%n", idx.val().getMostSignificantBits(), idx.i()));
+	public void testCorrectIndexEnumerateInline() {
+		final int numItems = 100;
+		final List<Idx<UUID>> idxList =  Stream.generate(UUID::randomUUID).limit(numItems)
+				.map(Idx.enumerate())
+				.collect(Collectors.toList());
+		for (int i = 0; i < numItems; i++) {
+			assertEquals("Expected the list index and Idx.i() values to be the same",
+					i, idxList.get(i).i());
+		}
+	}
+
+	@Test
+	public void testCorrectIndexEnumerateWrapper() {
+		final int numItems = 100;
+		final List<Idx<UUID>> idxList =  Idx.enumerate(
+				Stream.generate(UUID::randomUUID).limit(numItems))
+				.collect(Collectors.toList());
+		for (int i = 0; i < numItems; i++) {
+			assertEquals("Expected the list index and Idx.i() values to be the same",
+					i, idxList.get(i).i());
+		}
+	}
+
+	@Test
+	public void testCorrectIndexMapValOnly() {
+		final int numItems = 100;
+		final List<Idx<String>> idxList =  Stream.generate(UUID::randomUUID).limit(numItems)
+				.map(Idx.enumerate())
+				.map(Idx.Map(UUID::toString))
+				.collect(Collectors.toList());
+		for (int i = 0; i < numItems; i++) {
+			assertEquals("Expected the list index and Idx.i() values to be the same",
+					i, idxList.get(i).i());
+		}
+	}
+	@Test
+	public void testCorrectIndexMapBoth() {
+		final int numItems = 100;
+		final List<Idx<String>> idxList =  Stream.generate(UUID::randomUUID).limit(numItems)
+				.map(Idx.enumerate())
+				.map(Idx.Map( (i, val) -> val.toString()))
+				.collect(Collectors.toList());
+		for (int i = 0; i < numItems; i++) {
+			assertEquals("Expected the list index and Idx.i() values to be the same",
+					i, idxList.get(i).i());
+		}
 	}
 }
