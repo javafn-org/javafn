92820f7f65bcffb4f579c81afc4ab2ada1a68326
Ben Stringer <ben.stringer@usm.edu>
2026-04-06 01:59:55 -0500

    Adding deprecation warning for javafn-tuples

---
+++ src/main/java/org/javafn/tuple/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The classes in this project have been moved to a new project.
+ * <a href="https://github.com/javafn-org/javafn-tuples">https://github.com/javafn-org/javafn-tuples</a>
+ */
+@Deprecated
+package org.javafn.tuple;
