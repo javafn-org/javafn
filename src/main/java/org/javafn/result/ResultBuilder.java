92b8fd2d0fee8191f22e763c44abacf841f66722
Ben Stringer <ben.stringer@nrlssc.navy.mil>
2026-04-18 23:21:10 -0500

    Failed to incorporate in merge

--- src/main/java/org/javafn/result/ResultBuilder.java
+++ src/main/java/org/javafn/result/ResultBuilder.java
@@ -97,10 +97,12 @@ public class ResultBuilder<B> {
 	private final List<AnyError> errors = new ArrayList<>();
 	private final List<TypedValue<?,?,?>> oks = new ArrayList<>();
 
-	public <T> ResultBuilder<B> with(final TypedField<T> field, Try.ThrowingSupplier<T >fn) {
-		final Result<Exception, T> res = Try.get(fn);
+	public <T> ResultBuilder<B> with(final TypedField<T> field, Try.ThrowingSupplier<Result<AnyError, T>>fn) {
+		final Result<AnyError, T> res = Try.get(fn)
+				.asErr().map(AnyError::from)
+				.asOk().flatMap(Function.identity());
 		if (res.isErr) {
-			errors.add(new ExceptionError<>(res.asErr()));
+			errors.add(res.asErr().get());
 		} else {
 			oks.add(new TypedFieldValue<>(field, res.asOk().get()));
 		}
